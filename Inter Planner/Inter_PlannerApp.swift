7c5e7fbf7251a02cd650b7852ce3896664af4c1a
Vlad <mavrodimakis@icloud.com>
2026-08-24 17:57:32 +0300

    Initial Commit

---
+++ Inter Planner/Inter_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inter_PlannerApp.swift
+//  Inter Planner
+//
+//  Created by Vladyslav Mavrodimaki on 24/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Inter_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
