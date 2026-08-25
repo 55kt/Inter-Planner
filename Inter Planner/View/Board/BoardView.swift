1739bfc38458198c05031a4c64df2367393909b3
Vlad <mavrodimakis@icloud.com>
2026-08-25 18:44:52 +0300

    feat: add color palette assets for light and dark theme

--- Inter Planner/View/Board/BoardView.swift
+++ Inter Planner/View/Board/BoardView.swift
@@ -14,24 +14,24 @@ struct BoardView: View {
         ZStack {
             Canvas { context, size in
                 //Board background color
-                context.fill(Path(CGRect(origin: .zero, size: size)), with: .color(.white))
+                context.fill(Path(CGRect(origin: .zero, size: size)), with: .color(.boardBackground))
                 
                 // MARK: - Board Dots
                 /// Dots spacing
                 let r: CGFloat = 1.5
-                let spacing: CGFloat = 40
+                let spacing: CGFloat = 27
                 
                 /// Square dots
                 for x in stride(from: 0, through: size.width, by: spacing) {
                     for y in stride(from: 0, through: size.height, by: spacing) {
                         let rect = CGRect(x: x - r, y: y - r, width: 2*r, height: 2*r)
-                        context.fill(Path(ellipseIn: rect), with: .color(.black))
+                        context.fill(Path(ellipseIn: rect), with: .color(.boardDots))
                         
                         /// Central dot
                         let centerX = x + spacing/2
                         let centerY = y + spacing/2
                         let centerRect = CGRect(x: centerX - r, y: centerY - r, width: 2*r, height: 2*r)
-                        context.fill(Path(ellipseIn: centerRect), with: .color(.black))
+                        context.fill(Path(ellipseIn: centerRect), with: .color(.boardDots))
                     }
                 }
             }
